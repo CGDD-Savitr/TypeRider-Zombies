25ceff17c42711bc9b2c256de9862dd3f74761f9
Gunnar Örn Baldursson <gunnarorn.91@gmail.com>
2018-12-04 13:49:49 +0000

    Pulled, updated version to Unity 2018, implemented focus script

---
+++ Assets/Scripts/AutoFocusInput.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AutoFocusInput 	: MonoBehaviour {
+	void Start () {
+		GameObject obj = GameObject.FindGameObjectWithTag("PlayerInput");
+		if (obj != null)
+		{
+			InputField inputField = obj.GetComponent<InputField>();
+			if (inputField != null)
+			{
+				Debug.Log("PlayerInput selected");
+				inputField.Select();
+				inputField.ActivateInputField();
+			}
+			else
+			{
+				Debug.Log("PlayerInput is not InputField");
+			}
+		}
+		else 
+		{
+			Debug.Log("No component with PlayerInput tag");
+		}
+	}
+}
