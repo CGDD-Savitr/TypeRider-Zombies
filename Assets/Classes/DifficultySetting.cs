4101b83f3198a9f996c4273b513c56d174c34c19
Gunnar Örn Baldursson <gunnarorn.91@gmail.com>
2018-12-06 14:35:47 +0000

    Fix default value for difficulty setting

--- Assets/Classes/DifficultySetting.cs
+++ Assets/Classes/DifficultySetting.cs
@@ -1,5 +1,17 @@
 ﻿
 public static class DifficultySetting {
 
-	public static float Difficulty { get; set; }
+	public static float Difficulty
+	{
+		get
+		{
+			return difficulty;
+		}
+		set
+		{
+			difficulty = value;
+		}
+	}
+
+	private static float difficulty = 1f;
 }
