30d1afe01e8793a2e55bdaafcb8c03fcc2f41e6b
daniels15 <daniels15@ru.is>
2018-12-12 23:11:49 +0000

    coin model, 2 new floors, visual changes

--- Assets/Scripts/SpawnFloors.cs
+++ Assets/Scripts/SpawnFloors.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class SpawnFloors : MonoBehaviour {
 
 	public Transform floorParent;
-	public GameObject floorPrefab;
+	public GameObject[] floorPrefabs;
 	public float floorLength = 40f;
 
 	private Vector3 otherFloor;
+	private bool spaceFloorWasLast = false;
+	private int rand;
 
 	void OnTriggerEnter(Collider other)
 	{
@@ -16,11 +18,32 @@ public class SpawnFloors : MonoBehaviour {
 		{
 			case "FloorTrigger":
 				otherFloor = other.transform.parent.position;
-				Instantiate(
-					floorPrefab,
-					new Vector3(otherFloor.x, otherFloor.y, otherFloor.z + floorLength),
-					Quaternion.identity, floorParent
+				rand = (int)Random.Range(0f, 2.99f);
+
+				if (spaceFloorWasLast)
+				{
+					Instantiate(
+						floorPrefabs[1],
+						new Vector3(otherFloor.x, otherFloor.y, otherFloor.z + floorLength),
+						Quaternion.identity, floorParent
 					);
+					spaceFloorWasLast = false;
+				}
+				else
+				{
+					Instantiate(
+						floorPrefabs[rand],
+						new Vector3(otherFloor.x, otherFloor.y, otherFloor.z + floorLength),
+						Quaternion.identity, floorParent
+					);
+
+					if (rand == 2)
+					{
+						spaceFloorWasLast = true;
+					}
+
+				}
+				
 				break;
 		}
 	}
